7cdd7963ce46822d050534892b9319ad4f012496
Buted <993926354@qq.com>
2021-11-11 22:31:33 -0500

    Delete meta-class and other-class from Head model

--- code/dataloader/weighted_dataloader.py
+++ code/dataloader/weighted_dataloader.py
@@ -6,6 +6,7 @@ from torch.utils.data.dataloader import DataLoader
 
 from code.dataloader.ace_dataloader import ACE_Dataset, Batch_reader
 from code.config import Hyper
+from code.dataloader.few_role_dataloader import FewRole_Dataset
 
 
 class FewRoleWithOther_Dataset(ACE_Dataset):
@@ -25,22 +26,13 @@ class FewRoleWithOther_Dataset(ACE_Dataset):
         return role_remap
 
 
-class HeadRole_Dataset(FewRoleWithOther_Dataset):
+class HeadRole_Dataset(FewRole_Dataset):
     def __init__(self, hyper: Hyper, dataset: str, select_roles: List[int]):
-        self.role_vocab_size = hyper.role_vocab_size
-        super(HeadRole_Dataset, self).__init__(hyper, dataset, select_roles)
-
-    def _build_role_remap(self, select_roles: List[int]) -> Dict[int, int]:
-        role_remap = defaultdict(int)
-        for r in select_roles:
-            role_remap[r] = 0
-
-        j = 1
-        for i in range(self.role_vocab_size):
-            if i not in select_roles:
-                role_remap[i] = j
-                j += 1
-        return role_remap
+        reverse_roles = list(range(hyper.role_vocab_size))
+        for role in select_roles:
+            reverse_roles.remove(role)
+        reverse_roles.remove(0)
+        super(HeadRole_Dataset, self).__init__(hyper, dataset, reverse_roles)
 
 
 class Recall_Dataset(FewRoleWithOther_Dataset):
--- code/metrics/head_f1.py
+++ code/metrics/head_f1.py
@@ -1,6 +1,3 @@
-from typing import Dict, List
-from sklearn.metrics import classification_report
-
 from code.metrics.F1 import F1
 from code.config import Hyper
 
@@ -8,5 +5,6 @@ from code.config import Hyper
 class HeadF1(F1):
     def __init__(self, hyper: Hyper):
         super(HeadF1, self).__init__(hyper)
-        self.valid_labels = list(range(hyper.role_vocab_size - len(hyper.meta_roles) + 1))
-        self.valid_labels.remove(1)
+        self.valid_labels = list(range(hyper.role_vocab_size - len(hyper.meta_roles) - 1))
+        # self.valid_labels = list(range(hyper.role_vocab_size - len(hyper.meta_roles)))
+        # self.valid_labels.remove(0)
